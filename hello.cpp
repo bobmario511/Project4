585e01d76eefaa1f08f663ab5b9b1938e0e36aac
bobmario511 <williasc@seawolf.sonoma.edu>
2014-10-30 09:36:22 -0700

    Added a comment

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 using namespace std;
-int main(int argv, char * argv[])
+int main(int argc, char * argv[])
 {
-   cout << "Hello, " << argv[10] << endl;
+  if (argc != 1)
+    cout << "Hello, World" << endl;
+  else
+  cout << "Hello, " << argv[10] <<"!" << endl;
   return 0;
 }
